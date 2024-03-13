3ce92c51b8814b97ba7728c3f679900697210ce8
GabrielNScepanski <161836677+GabrielNScepanski@users.noreply.github.com>
2024-03-13 12:34:04 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+const primeironumero=10;
+const segundoNumero=20
+console.log(primeironumero + segundoNumero);
+
+const primeiroDECIMAL=3.14;
+const segundoDECIMAL=.5
+console.log(primeiroDECIMAL / segundoDECIMAL);
+console.log(primeiroDECIMAL - segundoDECIMAL);
+console.log(primeiroDECIMAL * segundoDECIMAL);
+
+const divisao=primeiroDECIMAL/segundoDECIMAL
+console.log(divisao)
---
+++ tipo-texto.js
@@ -0,0 +1,7 @@
+const nomeAluno="Beatriz";
+const idadeAluno="92";
+const MaeAluno="Juliana";
+
+console.log(nomeAluno)
+console.log(nomeAluno+idadeAluno)
+console.log(nomeAluno+MaeAluno)
